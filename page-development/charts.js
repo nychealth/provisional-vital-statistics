c66ca74f808a41e10b63fc90c66222a67beb9073
Morgan Mueller <84867869+mmueller1-NYCDOHMH@users.noreply.github.com>
2024-09-23 23:18:07 -0400

    Update charts.js
    modularized and added new spec/schema

--- page-development/charts.js
+++ page-development/charts.js
@@ -10,7 +10,7 @@
 
 
 // MAIN CHART-DRAWING FUNCTION
-function drawChart(destination, metric, label, multi, tooltip) {
+async function drawChart(destination, metric, label, multi, tooltip, schemaFlag = "default", selectedCauses = []) {
   /*
         Arguments:
             - destination: the ID of the vis container
@@ -18,28 +18,19 @@ function drawChart(destination, metric, label, multi, tooltip) {
             - label is a string that determines the chart's subtitle/y-axis label
             - multi is a true/false: false sets fill, true inserts the 'color' mark property on encoding
             - tooltip is a string that determines the label in the tooltip
+            - schemaFlag (optional) is a string that determines which schema to use. Defaults to "default".
+            - selectedCauses: array of selected causes for dynamic filtering.
 
-    */
+   */
 
   // INITIALIZE VARIABLES
-  var fillColor;
-  var subSeries;
-  var tooltipLabel = "";
-  var tooltipField = "";
+  let fillColor = multi === false ? "#f3f3f3" : "#f3f3f300";
+  let subSeries = multi === false ? "" : "submetric";
+  let tooltipLabel = tooltip ? tooltip : "";
+  let tooltipField = tooltip ? "submetric" : "";
 
-  multi === false ? (fillColor = "#f3f3f3") : (fillColor = "#f3f3f300");
-  multi === false ? (subSeries = "") : (subSeries = "submetric");
-
-  if (tooltip) {
-    tooltipLabel = tooltip;
-    tooltipField = "submetric";
-  } else {
-    tooltipLabel = "";
-    tooltipField = "";
-  }
-
-  // CHART SPEC
-  var spec = {
+  // DEFAULT SCHEMA
+  var spec1 = {
     $schema: "https://vega.github.io/schema/vega-lite/v5.json",
     description: "",
     height: 250,
@@ -140,7 +131,7 @@ function drawChart(destination, metric, label, multi, tooltip) {
         mark: { type: "text", align: "left", dx: 5 },
         encoding: {
           x: { aggregate: "max", timeUnit: "quarteryear", field: "date" },
-          y: { aggregate: {"argmax": "date"}, field: "value", type: "quantitative" },
+          y: { aggregate: { "argmax": "date" }, field: "value", type: "quantitative" },
           text: { field: "submetric", type: "nominal" },
           color: { field: "submetric", type: "nominal" },
         },
@@ -148,61 +139,197 @@ function drawChart(destination, metric, label, multi, tooltip) {
     ],
   };
 
-  // RUN VEGA EMBED
-  vegaEmbed(destination, spec, { actions: true });
-  // vegaEmbed("#bbd",spec)
+  // AlT SCHEMA
+  const spec2 = {
+    "$schema": "https://vega.github.io/schema/vega-lite/v5.json",
+    "description": "",
+    "height": 250,
+    "width": "container",
+    "title": {
+      "text": label,
+      "subtitlePadding": 10,
+      "fontWeight": "normal",
+      "anchor": "start",
+      "fontSize": 12,
+      "font": "sans-serif",
+      "baseline": "top",
+      "dy": -10,
+      "subtitleFontSize": 13
+    },
+    "config": {
+      "view": { "stroke": null },
+      "axisX": {
+        "labelAngle": 0,
+        "grid": false,
+        "tickSize": {
+          "condition": {
+            "test": { "field": "value", "timeUnit": "quarter", "equal": 1 },
+            "value": 15
+          },
+          "value": 9
+        },
+        "tickWidth": {
+          "condition": {
+            "test": { "field": "value", "timeUnit": "quarter", "equal": 1 },
+            "value": 1.25
+          },
+          "value": 0.5
+        },
+        "labelExpr": "[quarter(datum.value) === 1 ? timeFormat(datum.value, '%Y') + ' Q' + quarter(datum.value) : 'Q' + quarter(datum.value)]"
+      },
+      "axisY": { "tickCount": 2 },
+      "legend": {
+        "disable": true
+      }
+    },
+    "data": {
+      "url": "../data.csv",
+      "format": { "type": "csv" }
+    },
+    "transform": [
+      {
+        "filter": {
+          "field": "metric",
+          "equal": "Deaths by cause"
+        }
+      },
+      {
+        "filter": {
+          "field": "submetric",
+          "oneOf": selectedCauses
+        }
+      }
+    ],    
+    "layer": [
+      {
+        "mark": "line",
+        "encoding": {
+          "x": { "field": "date", "type": "temporal", "title": "Date" },
+          "y": { "field": "value", "type": "quantitative", "title": "Deaths" },
+          "color": { "field": "submetric", "type": "nominal", "title": "Cause of Death" }
+        }
+      },
+      {
+        "mark": { "type": "text", "align": "left", "dx": 5 },
+        "encoding": {
+          "x": { "aggregate": "max", "field": "date", "type": "temporal" },
+          "y": { "aggregate": { "argmax": "date" }, "field": "value", "type": "quantitative" },
+          "text": { "field": "submetric", "type": "nominal" },
+          "color": { "field": "submetric", "type": "nominal" }
+        }
+      }
+    ]
+  };
+  
+
+  const chosenSpec = schemaFlag === "alternative" ? spec2 : spec1;
+
+  try {
+    // RUN VEGA EMBED
+    await vegaEmbed(destination, chosenSpec, { actions: true });
+  } catch (error) {
+    console.log('Error embedding chart:', error);
+  }
 }
 
+
+
+
+// CREATE CHART CONFIGS
+
+const chartConfigs = [
+  { destination: '#bbd', metric: 'Total births', label: 'Births', multi: false, schemaFlag: "default" },
+  { destination: '#bbc', metric: 'By method', label: 'Percent of births', multi: true, schemaFlag: "default" },
+  { destination: '#dim', metric: 'IMR', label: 'Infant mortality rate (per 1,000 live births)', multi: false, schemaFlag: "default" },
+  { destination: '#ddc', metric: 'Deaths', label: 'Deaths', multi: false, schemaFlag: "alternative"  },
+];
+
 // INITIALIZE CHART DRAWS
-drawChart("#bbd", "Total births", "Births", false);
-
-drawChart('#bbc','By method','Percent of births',true)
-
-
-drawChart('#dim','IMR','Infant mortality rate (per 1,000 live births)',false)
-
-drawChart('#ddc','Deaths','Deaths',false)
+chartConfigs.forEach(({ destination, metric, label, multi, schemaFlag }) => {
+  drawChart(destination, metric, label, multi, schemaFlag);
+});
 
 
 
+// FUNCTIONALISED EVENT LISTENER 
+
+function addClickListeners(elements) {
+  elements.forEach((element) => {
+    element.addEventListener('click', function() {
+      elements.forEach(el => el.classList.remove('highlight'));
+      this.classList.add('highlight');
+    });
+  });
+}
 // EVENT LISTENERS FOR EACH CHART SECTION
-const demogElements = document.querySelectorAll('.byDemog');
-demogElements.forEach((element) => {
-  element.addEventListener('click', function() {
-    demogElements.forEach(el => el.classList.remove('highlight'));
-    this.classList.add('highlight');
-  });
-});
+addClickListeners(document.querySelectorAll('.byDemog'));
+addClickListeners(document.querySelectorAll('.birthChar'));
+addClickListeners(document.querySelectorAll('.care'));
+addClickListeners(document.querySelectorAll('.infantMort'));
+addClickListeners(document.querySelectorAll('.mort'));
 
-const birthChar = document.querySelectorAll('.birthChar');
-birthChar.forEach((element) => {
-  element.addEventListener('click', function() {
-    birthChar.forEach(el => el.classList.remove('highlight'));
-    this.classList.add('highlight');
-  });
-});
 
-const careChar = document.querySelectorAll('.care');
-careChar.forEach((element) => {
-  element.addEventListener('click', function() {
-    careChar.forEach(el => el.classList.remove('highlight'));
-    this.classList.add('highlight');
-  });
-});
+// GENERATE CHECKBOXES FOR DEATH BY CAUSE DROPDOWN
+function generateCheckboxes() {
+  fetch('../data.csv')
+    .then(response => response.text())
+    .then(csvText => {
+      const parsedData = d3.csvParse(csvText);
+      const deathsByCauseData = parsedData.filter(d => d.metric === "Deaths by cause");
+      const uniqueCauses = [...new Set(deathsByCauseData.map(d => d.submetric))];
+      const checkboxContainer = document.getElementById('checkboxContainer');
+      
+      checkboxContainer.innerHTML = "";
 
-const infantMort = document.querySelectorAll('.infantMort');
-infantMort.forEach((element) => {
-  element.addEventListener('click', function() {
-    infantMort.forEach(el => el.classList.remove('highlight'));
-    this.classList.add('highlight');
-  });
-});
+      uniqueCauses.forEach((cause, index) => {
+        const checkboxId = `cause${index + 1}`;
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.id = checkboxId;
+        checkbox.value = cause;
+        checkbox.checked = index < 5; // Check first 5 causes by default
+        
+        const label = document.createElement('label');
+        label.htmlFor = checkboxId;
+        label.innerText = cause;
 
-const mort = document.querySelectorAll('.mort');
-mort.forEach((element) => {
-  element.addEventListener('click', function() {
-    mort.forEach(el => el.classList.remove('highlight'));
-    this.classList.add('highlight');
+        checkboxContainer.appendChild(checkbox);
+        checkboxContainer.appendChild(label);
+
+        // EVENT LISTENER TO UPDATE CHART BASED ON SELECTION
+        checkbox.addEventListener('change', function() {
+          const selectedCauses = getSelectedCauses();
+          drawChart('#ddc', 'Deaths', 'Deaths', true, "", 'alternative', selectedCauses); 
+        });
+      });
+
+      // DRAW CHART WITH TOP 5 CAUSES OF DEATH
+      const initialSelection = uniqueCauses.slice(0, 5); 
+      drawChart('#ddc', 'Deaths', 'Deaths', true, "", 'alternative', initialSelection);
+    });
+}
+
+// CHOOSE CAUSES BASED ON SELECTION
+function getSelectedCauses() {
+  const checkboxes = document.querySelectorAll('#checkboxContainer input[type="checkbox"]');
+  return Array.from(checkboxes)
+    .filter(checkbox => checkbox.checked)
+    .map(checkbox => checkbox.value);
+}
+
+function toggleCauseDropdown(show) {
+  const dropdown = document.getElementById('causeDropdown');
+  dropdown.style.display = show ? 'inline-block' : 'none';
+}
+
+document.querySelectorAll('.mort').forEach((element) => {
+  element.addEventListener('click', function(e) {
+    if (e.target.innerText.includes('Cause')) {
+      toggleCauseDropdown(true);
+      generateCheckboxes(); 
+    } else {
+      toggleCauseDropdown(false);
+    }
   });
 });
 
