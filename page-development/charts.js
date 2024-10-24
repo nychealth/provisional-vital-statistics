cab266d856e839a98bc355f95d39adf475852361
M. Montesano <55593359+mmontesanonyc@users.noreply.github.com>
2024-10-24 09:43:51 -0400

    update copy

--- page-development/charts.js
+++ page-development/charts.js
@@ -1,11 +1,3 @@
-/*
-
- TO DO LIST:
-    - Add conditional for Percent - to format axisY
-    - Await proper copy
-    - Order legends
-*/
-
 // DETECT IF SUBMETRIC STARTS WITH NUMERIC VALUE
 function isNonNumeric(value) {
   return isNaN(parseInt(value.charAt(0)));
@@ -338,7 +330,7 @@ var partialConfig = {
 
   try {
     // RUN VEGA EMBED
-    await vegaEmbed(destination, chosenSpec, {actions: true });
+    await vegaEmbed(destination, chosenSpec, {actions: false });
   } catch (error) {
     console.log('Error embedding chart:', error);
   }
