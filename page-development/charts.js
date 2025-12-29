3252c590f69000d35b438a5efebae305465ed1a0
M. Montesano <55593359+mmontesanonyc@users.noreply.github.com>
2025-12-29 15:18:50 -0500

    initial chart label

--- page-development/charts.js
+++ page-development/charts.js
@@ -46,7 +46,8 @@ async function drawChart(destination, metric, label, multi, schemaFlag = "defaul
   let fillColor = multi === false ? "#f3f3f3" : "#f3f3f300";
   let subSeries = multi === false ? "" : "submetric";
   let fillLayer = multi === false ? [{"mark": {"type": "area", "color": "#f0f8ff85", "tooltip": false}}] : [];
-  let legend = multi === true ? {"columns": 6, "labelFontSize": 10, "symbolSize": 80} : {"disable": true};
+  let legend = multi === true ? {"columns": 6, "labelFontSize": 10, "labelFontWeight": "bold", "symbolType": "stroke", "labelColor": {"expr": "scale('color', datum.label)"}
+} : {"disable": true};
 
   // CREATE ARRAY OF SUBMETRICS TO PASS INTO SPEC.ENCODING.COLOR.SCALE.DOMAIN
   let groups = [];
@@ -139,7 +140,7 @@ var partialConfig = {
           "orient": "bottom", 
           "title": null, 
           "labelLimit": 1000,
-          "values": selectedCauses  // Filter the legend to only show selected causes for this chart
+          "values": selectedCauses  // Filter the legend to only show selected causes for this chart, 
         },
         "scale": {
           "domain": selectedCauses.length > 0 ? selectedCauses : sortedSubmetrics,  // Use selected causes if any, otherwise default
@@ -262,25 +263,138 @@ var partialConfig = {
       },
       {
         "transform": [
-          {
-            "aggregate": [
-              {"op": "argmin", "field": "date", "as": "value"},
-              {"op": "min", "field": "date", "as": "date"}
-            ],
-            "groupby": ["submetric"]
-          },
-          { "filter": "datum.value !== null && datum.value !== '' && datum.value !== 'null'" }
+        {
+          "joinaggregate": [
+            {"op": "max", "field": "value", "as": "maxChartValue"}
+          ]
+        },
+        {
+          "aggregate": [
+            {"op": "argmax", "field": "date", "as": "enddate"},
+            {"op": "max", "field": "date", "as": "date"}
+          ],
+          "groupby": ["submetric"]
+        },
+        {"calculate": "toNumber(datum.enddate.maxChartValue)", "as": "maxChartValue"},
+        {"calculate": "datum.maxChartValue * 0.05", "as": "minSep"},
+        {
+          "calculate": "toNumber(datum.enddate.value)",
+          "as": "value"
+        },
+        {
+          "filter": "datum.value !== null && datum.value !== '' && datum.value !== 'null'"
+        },
+        {
+          "calculate": "replace(datum.submetric, /(.{8,}?)(\\s+)/g, '$1\\n')",
+          "as": "submetric"
+        },
+        {
+          "window": [
+            {"op": "lag", "field": "enddate['value']", "as": "prevVal1"}
+          ],
+          "sort": [{"field": "enddate['value']", "order": "ascending"}]
+        },
+      {
+        "calculate": "toNumber(datum.prevVal1)",
+        "as": "prevVal1"
+      },
+      {
+        "calculate": "datum.prevVal1 === null ? 0 : datum.value - datum.prevVal1",
+        "as": "valDif1"
+      },
+      {
+        "calculate": "datum.prevVal1 === null ? datum.value*.9 : (datum.valDif1 < datum.minSep ? datum.value + datum.minSep : datum.value)",
+        "as": "offset1"
+      },
+
+        {
+    "window": [
+      {"op": "lag", "field": "enddate['value']", "as": "prevVal2"}
+    ],
+    "sort": [{"field": "enddate['value']", "order": "ascending"}]
+  },
+  {
+    "calculate": "toNumber(datum.prevVal2)",
+    "as": "prevVal2"
+  },
+  {
+    "calculate": "datum.prevVal2 === null ? 0 : datum.value - datum.prevVal2",
+    "as": "valDif2"
+  },
+  {
+    "calculate": "datum.prevVal2 == null ? datum.value*.9 : (datum.valDif2 < datum.minSep ? datum.value + datum.minSep : datum.value)",
+    "as": "offset2"
+  },
+
+  // --- Third pass ---
+  {
+    "window": [
+      {"op": "lag", "field": "enddate['value']", "as": "prevVal3"}
+    ],
+    "sort": [{"field": "enddate['value']", "order": "ascending"}]
+  },
+  {
+    "calculate": "toNumber(datum.prevVal3)",
+    "as": "prevVal3"
+  },
+  {
+    "calculate": "datum.prevVal3 === null ? 0 : datum.value - datum.prevVal3",
+    "as": "valDif3"
+  },
+  {
+    "calculate": "datum.prevVal3 == null ? datum.value*.9 : (datum.valDif3 < datum.minSep ? datum.value + datum.minSep : datum.value)",
+    "as": "offset3"
+  },
+  {
+  "window": [
+    {"op": "lag", "field": "enddate['value']", "as": "prevVal4"}
+  ],
+  "sort": [{"field": "enddate['value']", "order": "ascending"}]
+},
+{
+  "calculate": "toNumber(datum.prevVal4)",
+  "as": "prevVal4"
+},
+{
+  "calculate": "datum.prevVal4 === null ? 0 : datum.value - datum.prevVal4",
+  "as": "valDif4"
+},
+{
+  "calculate": "datum.prevVal4 == null ? datum.value*.9 : (datum.valDif4 < datum.minSep ? datum.value + datum.minSep : datum.value)",
+  "as": "offset4"
+},
+
+  {
+  "window": [
+    {"op": "lag", "field": "enddate['value']", "as": "prevVal5"}
+  ],
+  "sort": [{"field": "enddate['value']", "order": "ascending"}]
+},
+{
+  "calculate": "toNumber(datum.prevVal5)",
+  "as": "prevVal5"
+},
+{
+  "calculate": "datum.prevVal5 === null ? 0 : datum.value - datum.prevVal5",
+  "as": "valDif5"
+},
+{
+  "calculate": "datum.prevVal5 == null ? datum.value*.9 : (datum.valDif5 < datum.minSep ? datum.value + datum.minSep : datum.value)",
+  "as": "offset5"
+}
+
+
         ],
         "encoding": {
           "x": {"field": "date"},
-          "y": {"field": "value['value']"},
+          "y": {"field": "offset5"},
           "text": {
-            "condition": {"param": "hover", "field": "submetric", "empty": false},
-            "value": ""
+            "field": "submetric"
           },
+          "tooltip": [],
           "color": {"field": "submetric", "type": "nominal"}
         },
-        "mark": {"type": "text", "align": "left", "dx": -8, "dy": -15,"fontSize": 12, "fontWeight": "bold"}
+        "mark": {"type": "text", "align": "left", "dx": 8, "dy": -5,"fontSize": 10, "fontWeight": "bold","lineBreak": "\n",}
       }
     ]
   }
@@ -294,7 +408,7 @@ var partialConfig = {
     "title": title,
     "config": {
       ...partialConfig,
-      "legend": {"columns": 4, "labelFontSize": 10, "symbolSize": 80}
+      "legend": {"columns": 4, "labelFontSize": 10, "labelFontWeight": "bold", "symbolType": "stroke",  "labelColor": {"expr": "scale('color', datum.label)"}}
     },
     "data": {
       "url": dataSource
@@ -372,7 +486,7 @@ var partialConfig = {
 
   try {
     // RUN VEGA EMBED
-    await vegaEmbed(destination, chosenSpec, {actions: false });
+    await vegaEmbed(destination, chosenSpec, {actions: true });
   } catch (error) {
     console.log('Error embedding chart:', error);
   }
